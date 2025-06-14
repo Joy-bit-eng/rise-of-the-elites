13236baa7182577db8138d9c507e81feee632124
Joy-bit-eng <ls9988236@gmail.com>
2025-06-14 20:12:42 +0530

    Update PreloadScene.js

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -4,11 +4,28 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    // Load your assets here, e.g.:
-    // this.load.image('player', 'sprites/player.png');
+    // Background color
+    this.cameras.main.setBackgroundColor('#000');
+
+    // Show splash text
+    this.add.text(
+      this.scale.width / 2,
+      this.scale.height / 2,
+      'Rise of the Elites!',
+      {
+        font: '28px monospace',
+        fill: '#ffffff'
+      }
+    ).setOrigin(0.5);
+
+    // Example preload (add real assets later)
+    // this.load.image('player', 'assets/player.png');
   }
 
   create() {
-    this.scene.start('GameScene');
+    // After 2 seconds, start the main game
+    this.time.delayedCall(2000, () => {
+      this.scene.start('GameScene');
+    });
   }
 }
